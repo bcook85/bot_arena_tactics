13329c1a3f615bbf0128ffe0d91b4d5c2562bc38
Bryan Cook <bryandeancook@gmail.com>
2024-03-15 10:33:05 -0400

    Quick Menu Update Added menu class objects to temp states.

--- js/trainmenustate.js
+++ js/trainmenustate.js
@@ -3,43 +3,29 @@
 class TrainMenuState extends State {
   constructor(game) {
     super(game);
-    // Assets
-    this.indicatorLeftImage = this.game.gfx.ui.cut(0, 0, 32, 32);
-    this.indicatorRightImage = this.game.gfx.ui.cut(32, 0, 32, 32);
-    // Items
-    this.indicatorXOffset = 96;
-    this.itemSize = 32;
-    this.itemStartX = this.game.screenWidth * 0.5;
-    this.itemStartY = this.game.screenHeight * 0.25;
-    this.itemFont = `${this.itemSize}px Monospace`;
-    this.itemAlignment = "center";
-    this.itemBaseColor = "rgb(0,255,255)";
-    this.itemHighlightColor = "rgb(255,255,255)";
-    this.items = ["Resume", "Save", "Quit"];
-    this.selectedItem = 0;
+    // Menu
+    this.menu = new MenuVertical(
+      ["Resume", "Save", "Quit"],
+      18,
+      this.game.screenWidth * 0.5,
+      this.game.screenHeight * 0.5
+    );
   };
   update(dt) {
     // User Input
-    if (this.game.keys.isUp("Enter") || this.game.keys.isUp("f")) {
-      switch(this.items[this.selectedItem]) {
-        case "Resume":
-          this.leave();
-          break;
-        case "Save":
-          // stuff
-          break;
-        case "Quit":
-          this.reset();
-          break;
-        default:
-          break;
-      }
-    }
-    if (this.game.keys.isUp("ArrowUp") || this.game.keys.isUp("w")) {
-      this.selectedItem = Math.max(0, this.selectedItem - 1);
-    }
-    if (this.game.keys.isUp("ArrowDown") || this.game.keys.isUp("s")) {
-      this.selectedItem = Math.min(this.items.length - 1, this.selectedItem + 1);
+    let userChoice = this.menu.update(this.game.keys, this.game.mouse);
+    switch(userChoice) {
+      case "Resume":
+        this.leave();
+        break;
+      case "Save":
+        // stuff
+        break;
+      case "Quit":
+        this.reset();
+        break;
+      default:
+        break;
     }
   };
   render(ctx) {
@@ -55,33 +41,7 @@ class TrainMenuState extends State {
       ,Math.floor(this.game.screenWidth * 0.5)
       ,0
     );
-    // UI Items
-    ctx.font = this.itemFont;
-    ctx.textAlign = this.itemAlignment;
-    ctx.textBaseline = "top";
-    for (let i = 0; i < this.items.length; i++) {
-      let color = i == this.selectedItem ? this.itemHighlightColor : this.itemBaseColor;
-      ctx.fillStyle = color;
-      ctx.fillText(
-        this.items[i]
-        ,this.itemStartX
-        ,this.itemStartY + ((i + 1) * this.itemSize)
-      );
-    }
-    let indicatorY = this.itemStartY + ((this.selectedItem + 1) * this.itemSize);
-    ctx.drawImage(
-      this.indicatorLeftImage,
-      this.itemStartX - this.indicatorXOffset - this.itemSize,
-      indicatorY,
-      this.itemSize,
-      this.itemSize
-    );
-    ctx.drawImage(
-      this.indicatorRightImage,
-      this.itemStartX + this.indicatorXOffset,
-      indicatorY,
-      this.itemSize,
-      this.itemSize
-    );
+    // Menu
+    this.menu.render(ctx);
   };
 };
